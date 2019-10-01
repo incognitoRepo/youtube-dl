67aa2d19311a9ebe5701ea5e01d8d2b06333aebd
incognitoRepo <incognitoRepo@outlook.com>
2019-09-30 21:12:55 -0700

    21

--- devscripts/ytdev.zsh
+++ devscripts/ytdev.zsh
@@ -1,7 +1,6 @@
 # $ python -m youtube_dl          (2.7+)
 
-rm /Users/alberthan/VSCodeProjects/vytd/src/youtube-dl/youtube_dl/el155
-fd -d 1 '/Users/alberthan/VSCodeProjects/vytd/src/youtube-dl/youtube_dl/el[0-9]{3}'
+fd -d 1 'el[0-9]{3}' '/Users/alberthan/VSCodeProjects/vytd/src/youtube-dl/youtube_dl/' | xargs rm
 
 CFG="reference"
 /Users/alberthan/VSCodeProjects/vytd/bin/python3 -m youtube_dl --verbose https://www.pornhub.com/view_video.php\?viewkey\=ph5d5270a4e3bca
